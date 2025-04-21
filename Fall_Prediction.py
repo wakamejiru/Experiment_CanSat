eb7883c6cccc550152ca20b3f9c107d47164f5a5
wakamejiru <63459321+wakamejiru@users.noreply.github.com>
2025-04-21 14:55:28 +0900

    落下予測処理を追加
    cd_calc 落下係数を算出
    Fall_predicition 落下の時間などを予測

---
+++ Fall_Prediction.py
@@ -0,0 +1,70 @@
+import numpy as np
+air_density = 1.206 # 1気圧の空気密度
+parachute_fai = 0.18 # パラシュートの半径
+inner_fai = 0.05
+parachute_s = parachute_fai * parachute_fai * np.pi  - (inner_fai*inner_fai*np.pi) # パラシュートの面積
+drag_coefficent = 0.5 # 抵抗係数
+gravitational_acceleration = 9.80665
+
+def main():
+ weight = 0.079 # 単位はkg
+ hegiht = 5.9895
+ 
+ time, v = Freefall(hegiht, 0, weight)
+ print(f"落下時間: {time:.2f} 秒")
+ print(f"着地時の速度: {v:.2f} m/s")
+"""
+ 横向きと縦向きを考慮する力
+ 空気抵抗がある場合は速度が時々刻々と変化するため、加算で求める
+ height_now 求める時点の高さ
+ verocity_now_z 現在の速度
+ verocity_now_xy 横方向の力(スカラ量)(ロケットの横向きの力もここに加わる)
+ weight 落下物の重さ
+"""
+def Freefall2D(height_now, verocity_now_z, verocity_now_xy, weight, wind_speed):
+ # X方向の空気抵抗(風)
+ del_time = 0.1
+ height = height_now
+ verocity_xy = verocity_now_xy
+ g = gravitational_acceleration
+ time = 0
+ x_position = 0
+ while height > 0:
+  # x方向の空気抵抗（風を考慮）
+  Fd_x = 0.5 * air_density * (verocity_xy - wind_speed)**2 * drag_coefficent * parachute_s * np.sign(vx - wind_speed)
+  a_x = -Fd_x / weight
+  verocity_xy += a_x * del_time
+  x_position += verocity_xy * del_time
+
+  # z方向の重力と空気抵抗
+  Fd_z = 0.5* air_density * verocity_z**2 * drag_coefficent * parachute_s
+  a_z = g - Fd_z / weight
+  verocity_z += a_z * del_time
+  height -= verocity_z * del_time
+  time += del_time
+
+"""
+ 直線的な落下
+ 空気抵抗がある場合は速度が時々刻々と変化するため、加算で求める
+ height_now 求める時点の高さ
+ verocity_now_z 現在の速度
+ weight 落下物の重さ
+"""
+def Freefall(height_now, verocity_now_z, weight):
+ del_time = 0.1
+ height = height_now
+ verocity_z = verocity_now_z
+ g = gravitational_acceleration
+ time = 0
+ 
+ while height > 0:
+   Fd_z = 0.5* air_density * verocity_z**2 * drag_coefficent * parachute_s
+   a_z = g - Fd_z / weight
+   verocity_z += a_z * del_time
+   height -= verocity_z * del_time
+   time += del_time
+
+ return time, verocity
+
+if __name__ == "__main__":
+ main()
